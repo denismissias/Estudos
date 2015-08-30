c9cad7276e4c1cc006e06aa24fa9188830c4a5a8
Denis Missias <denis.missias@gmail.com>
2015-08-30 20:11:37 -0300

    Estudo dos operadores matemáticos

--- Universidade XTI/Javascript/js/Aula09.js
+++ Universidade XTI/Javascript/js/Aula09.js
@@ -3,5 +3,14 @@
     Descrição: Estudo dos operadores javascript
 */
 
-var x = + "-2";
+var x = + "-2"; //Conversão de String para Number
 alert(x);
+
+var a = 6;
+var b = a++;
+
+//++a - Pré incremento
+//a++ - Pós incremento
+
+alert("a " + a);
+alert("b " + b);
