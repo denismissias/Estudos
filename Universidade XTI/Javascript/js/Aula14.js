6e56f30527dd0941160fab234ccaefccfa26a95f
Denis Missias <denis.missias@gmail.com>
2015-09-05 02:07:31 -0300

    Inclusão dos estilos

--- Universidade XTI/Javascript/js/Aula14.js
+++ Universidade XTI/Javascript/js/Aula14.js
@@ -1,7 +1,11 @@
 function calcularIMC() {
     var formulario = document.getElementById("formulario");
     
-    var kilos = formulario.kilos.value;
-    var metros = formulario.metros.value;
-    var centimetros = formulario.centimetros.value;
+    var kilos = parseInt(formulario.kilos.value);
+    var metros = parseInt(formulario.metros.value);
+    var centimetros = parseInt(formulario.centimetros.value);
+    var altura = ((metros * 100) + centimetros) / 100;
+    var imc = kilos / (altura * altura);
+    
+    formulario.imc.value = imc.toFixed(2);
 }
