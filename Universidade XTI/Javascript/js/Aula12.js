3bfa913e087edb1329e341634bb1b35a7db88cd7
Denis Missias <denis.missias@gmail.com>
2015-09-05 01:21:16 -0300

    Commit inicial

---
+++ Universidade XTI/Javascript/js/Aula12.js
@@ -0,0 +1,12 @@
+/*
+    Autor: Denis de Sousa Missias
+    Descrição: Estudo dos operadores javascript
+*/
+
+var x = 6;
+
+x /= 3;
+alert(x);
+
+//== Compara somente valor
+//=== Compara valor e tipo
