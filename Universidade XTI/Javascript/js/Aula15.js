72861f00199d5150a83af65175af2ede11a6520a
Denis Missias <denis.missias@gmail.com>
2015-09-05 14:32:56 -0300

    Declarações de estruturas function

--- Universidade XTI/Javascript/js/Aula15.js
+++ Universidade XTI/Javascript/js/Aula15.js
@@ -1,11 +1,5 @@
-function calcularIMC() {
-    var formulario = document.getElementById("formulario");
-    
-    var kilos = parseInt(formulario.kilos.value);
-    var metros = parseInt(formulario.metros.value);
-    var centimetros = parseInt(formulario.centimetros.value);
-    var altura = ((metros * 100) + centimetros) / 100;
-    var imc = kilos / (altura * altura);
-    
-    formulario.imc.value = imc.toFixed(2);
+function dividir(x, y) {
+    return (x/y);
 }
+
+alert(dividir(6, 2));
