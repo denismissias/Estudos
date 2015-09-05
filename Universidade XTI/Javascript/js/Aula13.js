4d6e4454f875b850edcefd9fd79070fe4e2eceab
Denis Missias <denis.missias@gmail.com>
2015-09-05 01:37:35 -0300

    Finalização dos operadores

--- Universidade XTI/Javascript/js/Aula13.js
+++ Universidade XTI/Javascript/js/Aula13.js
@@ -2,11 +2,26 @@
     Autor: Denis de Sousa Missias
     Descrição: Estudo dos operadores javascript
 */
+Pessoa = {
+    nome: "Denis"
+}
 
-var x = 6;
+alert(Pessoa.nome);
+alert("nome" in Pessoa);
+delete(Pessoa.nome);
+alert(Pessoa.nome);
 
-x /= 3;
+alert("Email" in Pessoa);
+
+var idade = new Number(31);
+
+alert(idade instanceof Number);
+var x = (idade >= 18) ? "Maior de idade" : "Menor de idade";
 alert(x);
+
+alert(this.document.title);
+
+alert(typeof(true));
 
 //== Compara somente valor
 //=== Compara valor e tipo
