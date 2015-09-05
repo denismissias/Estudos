8ba4e2a7a47976bc864658d95fcd25902792f206
Denis Missias <denis.missias@gmail.com>
2015-09-05 01:51:21 -0300

    Commit inicial

---
+++ Universidade XTI/Javascript/js/Aula14.js
@@ -0,0 +1,7 @@
+function calcularIMC() {
+    var formulario = document.getElementById("formulario");
+    
+    var kilos = formulario.kilos.value;
+    var metros = formulario.metros.value;
+    var centimetros = formulario.centimetros.value;
+}
