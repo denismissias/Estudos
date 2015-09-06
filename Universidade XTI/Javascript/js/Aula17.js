447b1e3a270128d39db7a6f0916e2bbd0f612c3e
Denis Missias <denis.missias@gmail.com>
2015-09-06 01:53:03 -0300

    Switch

---
+++ Universidade XTI/Javascript/js/Aula17.js
@@ -0,0 +1,13 @@
+var sexo = "M";
+
+switch (sexo) {
+    case "M":
+        alert("Masculino");
+        break;
+    case "F":
+        alert("Feminino");
+        break;
+    default:
+        alert("Indefinido");
+        break;
+}
