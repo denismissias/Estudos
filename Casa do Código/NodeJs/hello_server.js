3d54424d7993e7c337e2afc74a9b18bb86240376
Denis Missias <denis.missias@gmail.com>
2015-10-04 09:34:48 -0300

    Ajustes

--- Casa do Código/NodeJs/hello_server.js
+++ Casa do Código/NodeJs/hello_server.js
@@ -1,6 +1,8 @@
+'use strict';
+
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
+var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<h1>Hello World!</h1>");
     response.end();
