715d58add3085b526a4f921ceacdd5c2bfea90ff
Denis Missias <denis.missias@gmail.com>
2015-10-04 13:22:27 -0300

    Inclusão do tratamento do erro

--- Casa do Código/NodeJs/site_pessoal.js
+++ Casa do Código/NodeJs/site_pessoal.js
@@ -7,8 +7,12 @@ var server = http.createServer(function (request, response) {
     // A constante __dirname retorna o diretório raiz da aplicação.
     fs.readFile(__dirname + '/index.html', function (erro, html) {
         response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(html);
-        response.end();
+        if (erro) {
+            response.write("<h2>" + erro + "</h2>");
+        } else {
+            response.write(html);
+            response.end();
+        }
     });
 });
 
