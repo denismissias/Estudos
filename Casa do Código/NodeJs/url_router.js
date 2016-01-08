407331645c2ff10dd5ad7aad2590432df14b559f
denismissias <denis.missias@gmail.com>
2016-01-08 17:55:13 -0200

    Estudo dos capítulos e início do projeto ntalk

--- Casa do Código/NodeJs/url_router.js
+++ Casa do Código/NodeJs/url_router.js
@@ -1,25 +1,41 @@
-'use strict'
-
 var http = require('http');
-var url = require('url');
 var fs = require('fs');
+var url = require('url');
 
-var server = http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": 'txt/html'});
-    
-    var caminho = url.parse(request.url, true);
-    
-    if ((caminho.path == "/") || (caminho.path == "/artigos")) {
-        fs.readFile(__dirname + "/artigos.html", function (erro, html) {
-            if(fs.exists(html)) {
-                response.end(html);
-            } else {
-                response.end("<h2>Arquivo não encontrado</h2>");
-            }
-        });
-    };
+// Retorna o endereço local do arquivo no projeto.
+var diretorio = function(arquivo){
+  return __dirname + "/" + arquivo;
+};
+
+// Funçao simples de gerenciamento a rotas.
+var rotear = function(pathname){
+  if(pathname && pathname != "/"){
+    var arquivo = diretorio(pathname + ".html");
+    var existe = fs.existsSync(arquivo);
+    if(existe){
+      return arquivo;
+    }
+    return diretorio("erro.html");
+  }
+  return diretorio("artigos.html");
+};
+
+// Iniciando Servidor do desafio
+var server = http.createServer(function(request, response){
+
+  // Obtendo o pathname digitado no browser
+  var pathname = url.parse(request.url).pathname;
+  
+  // Processando roteamento do pathname
+  var pagina = rotear(pathname);
+
+  // Renderizando a pagina html
+  fs.readFile(pagina, function(err, html){
+    response.writeHeader(200, {'Content-Type': 'text/html'});
+    response.end(html);
+  });
 });
 
-server.listen(3000, function () {
-    console.log("URL Router is running");
+server.listen(3000, function(){
+  console.log('Executando Desafio');
 });
