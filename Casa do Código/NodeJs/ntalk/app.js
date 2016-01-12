8b65dbcdc89465a003151f814105967401d4c50d
denismissias <denis.missias@gmail.com>
2016-01-12 12:22:38 -0200

    Finalização do capítulo

--- Casa do Código/NodeJs/ntalk/app.js
+++ Casa do Código/NodeJs/ntalk/app.js
@@ -6,6 +6,10 @@ var load = require('express-load');
 var app = express();
 
 // view engine setup
+app.use(express.cookieParser('ntalk'));
+app.use(express.session());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
