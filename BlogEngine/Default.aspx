e07f6473138fd3e50e1713ef95587df5d20b9e4b
Denis de Sousa Missias <denismissias@hotmail.com>
2016-02-27 18:09:16 -0300

    Commit inicial

---
+++ BlogEngine/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BlogEngine.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>BlogEngine - Gerenciador de Blogs</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>BlogEngine - Gerenciador de Blogs</h1>
+
+        Bem vindo ao gerenciador de blogs.
+        <br />
+        Faça o seu login nos campos abaixo ou, crie o seu blog <a href="NewBlog.aspx">aqui</a>.
+        <br />
+
+        E-Mail
+        <br />
+        <asp:TextBox runat="server" ID="txtEmail" Columns="100" MaxLength="100"></asp:TextBox>
+        <br />
+        <asp:RequiredFieldValidator runat="server" ID="rfvLogin" ErrorMessage="É necessário informar um e-mail." Display="Dynamic" ControlToValidate="txtEmail"></asp:RequiredFieldValidator>
+        <br />
+
+        Senha
+        <br />
+        <asp:TextBox runat="server" ID="txtPassword" Columns="10" MaxLength="10" TextMode="Password"></asp:TextBox>
+        <br />
+        <asp:RequiredFieldValidator runat="server" ID="rfvPassword" ErrorMessage="É necessário informar a senha." Display="Dynamic" ControlToValidate="txtPassword"></asp:RequiredFieldValidator>
+        <br />
+
+        <asp:Button runat="server" ID="btnSend" Text="Enviar" OnClick="btnSend_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ BlogEngine/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI;
+
+namespace BlogEngine
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack && Session["UserId"] != null)
+            {
+                Session["UserId"] = null;
+            }
+        }
+
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            if (!Page.IsValid)
+            {
+                return;
+            }
+
+            doLogin();
+        }
+
+        private void doLogin()
+        {
+            BlogEngineEntities dataModel = new BlogEngineEntities();
+
+            var result = (from blog in dataModel.blogs
+                          where blog.admin_mail == txtEmail.Text.ToLower() &&
+                                blog.password == txtPassword.Text
+                          select new
+                                {
+                                    blog.id
+                                }).ToList();
+
+            if (result == null || result.Count == 0)
+            {
+                ClientScript.RegisterStartupScript(Page.GetType(), "Login", "<script>alert('Login ou senha incorretos.')</script>");
+                return;
+            }
+
+            Session["UserId"] = result[0].id;
+            Response.Redirect("BlogStartPage.aspx");
+        }
+    }
+}
