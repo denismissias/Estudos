fa47a8a427304ac264c69ab6b166e2f34bace378
Denis de Sousa Missias <denismissias@hotmail.com>
2016-03-18 22:44:18 -0300

    Refatoração

--- System.IOExample/Program.cs
+++ System.IOExample/Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 
 namespace System.IOExample
 {
@@ -16,7 +12,7 @@ namespace System.IOExample
 
             File.AppendAllText(@"C:\Users\Denis Missias\Desktop\1.txt", sb.ToString());
 
-            String [] values = {"Line 1", "Line 2", "Line 3"};
+            String[] values = { "Line 1", "Line 2", "Line 3" };
 
             File.AppendAllLines(@"C:\Users\Denis Missias\Desktop\2.txt", values);
 
