29d334ef1fd4db7f62607cb44e1d16131b731ade
Denis de Sousa Missias <denismissias@hotmail.com>
2016-03-19 17:50:24 -0300

    Create e update de nós com atributos

---
+++ System.XmlExample/Form2.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace System.XmlExample
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAgenda = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAgenda
+            // 
+            this.lblAgenda.AutoSize = true;
+            this.lblAgenda.Location = new System.Drawing.Point(139, 13);
+            this.lblAgenda.Name = "lblAgenda";
+            this.lblAgenda.Size = new System.Drawing.Size(0, 13);
+            this.lblAgenda.TabIndex = 0;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(13, 13);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(13, 30);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(13, 57);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(49, 13);
+            this.lblTelefone.TabIndex = 3;
+            this.lblTelefone.Text = "Telefone";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(13, 74);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 4;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(13, 101);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 5;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblAgenda);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Windows.Forms.Label lblAgenda;
+        private Windows.Forms.Label lblNome;
+        private Windows.Forms.TextBox txtNome;
+        private Windows.Forms.Label lblTelefone;
+        private Windows.Forms.TextBox txtTelefone;
+        private Windows.Forms.Button btnSalvar;
+    }
+}
---
+++ System.XmlExample/Form2.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace System.XmlExample
+{
+    public partial class Form2 : Form
+    {
+        String arquivo = @"C:\Users\Denis Missias\Desktop\Agenda2.xml";
+        XmlDocument xmlDoc = new XmlDocument();
+
+        public Form2()
+        {
+            InitializeComponent();
+
+            if (!File.Exists(arquivo))
+            {
+                XmlNode nodeRoot = xmlDoc.CreateElement("Contatos");
+                xmlDoc.AppendChild(nodeRoot);
+                xmlDoc.Save(arquivo);
+            }
+            else
+            {
+                LerAgenda();
+            }
+        }
+
+        private void LerAgenda()
+        {
+            xmlDoc.Load(arquivo);
+
+            lblAgenda.Text = "Contatos:\n\n";
+
+            foreach (XmlNode node in xmlDoc.GetElementsByTagName("Contato"))
+            {
+                lblAgenda.Text += "Nome: " + node.Attributes["nome"].Value +
+                    " Telefone: " + node.Attributes["telefone"].Value + "\n";
+            }
+        }
+
+        private void AdicionarAgenda()
+        {
+            XElement xElement = new XElement("Contato");
+            xElement.Add(new XAttribute("nome", txtNome.Text));
+            xElement.Add(new XAttribute("telefone", txtTelefone.Text));
+
+            XElement file = XElement.Load(arquivo);
+            file.Add(xElement);
+            file.Save(arquivo);
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            AdicionarAgenda();
+
+            LerAgenda();
+
+            LimparForm();
+        }
+
+        private void LimparForm()
+        {
+            txtNome.Clear();
+            txtTelefone.Clear();
+        }
+    }
+}
