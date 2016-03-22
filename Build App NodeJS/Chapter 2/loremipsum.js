3fe0385e2823d629507d48cffb36812313caddea
root <root@debian>
2016-03-22 00:36:46 -0300

    Geração dinâmica do arquivo.

--- Build App NodeJS/Chapter 2/loremipsum.js
+++ Build App NodeJS/Chapter 2/loremipsum.js
@@ -1,7 +1,24 @@
+/*
+ * loremipsum.js
+ * 
+ * Faz uma requisição na API 'http://loripsum.net/api/' e grava um arquivo com o 
+ * nome e a quantidade de parágrafos especificados.
+ * 
+ * Denis Missias, Março de 2016 
+*/
+
 var http = require('http');
 var fs = require('fs');
+var fileName = String(process.argv[2] || '').replace(/[^a-z0-9\.]/gi, '');
+var quantityOfParagraphs = String(process.argv[3] || '').replace(/[^\d]/g, '');
 
-http.get('http://loripsum.net/api/1', function(res) {
+const USAGE = "USO: node loremipsum.js {nome do arquivo} {quantidade de parágrafos}";
+
+if (!fileName || !quantityOfParagraphs) {
+    return console.log(USAGE);
+}
+
+http.get('http://loripsum.net/api/' + quantityOfParagraphs, function(res) {
 	var text = '';
 
 	res.on('data', function (chunk) {
@@ -9,8 +26,8 @@ http.get('http://loripsum.net/api/1', function(res) {
 	});
 
 	res.on('end', function () {
-		fs.writeFile('lorem.html', text, function () {
-            console.log("Arquivo pronto!");
+		fs.writeFile(fileName, text, function () {
+            console.log("Arquivo " + fileName + " pronto!");
         });
 	});
 }).on('error', function(e) {
