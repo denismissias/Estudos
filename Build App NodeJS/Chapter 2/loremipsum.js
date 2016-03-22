7aee8e2836fd18a05737454140d2f48b7ba11dbd
root <root@debian>
2016-03-21 23:56:04 -0300

    Reorganização de pastas e inclusão do capítulo 2

---
+++ Build App NodeJS/Chapter 2/loremipsum.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+http.get('http://loripsumerro.net/api/1', function(res) {
+	var text = '';
+
+	res.on('data', function (chunk) {
+		text += chunk;
+	});
+
+	res.on('end', function () {
+		console.log(text);
+	});
+}).on('error', function(e) {
+	console.log('Got error: ' + e.message);
+});
