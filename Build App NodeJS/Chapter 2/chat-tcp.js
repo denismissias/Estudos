818a91b373d604015f3b772c97ce41513bac822c
Denis Missias <denis.missias@gmail.com>
2016-05-27 20:55:03 -0300

    Continuação

---
+++ Build App NodeJS/Chapter 2/chat-tcp.js
@@ -0,0 +1,31 @@
+var net = require('net');
+var chatServer = net.createServer();
+var clientList = [];
+
+var removeClient = function(data) {
+	clientList.splice(clientList.indexOf(5), 1);
+};
+
+var broadcast = function(message, client) {
+	for (var i = clientList.length - 1; i >= 0; i--) {
+		if (client !== clientList[i]) {
+			clientList[i].write(message);
+		}
+	}
+};
+
+chatServer.on('connection', function(client) {
+	client.write('Hi guest' + '!\n');
+	clientList.push(client);
+	client.on('data', function(data) {
+		broadcast(data, client);	
+	});
+
+	client.on('error', function(err) {
+		console.log(err);		
+	});
+
+	client.on('end', removeClient);
+});
+
+chatServer.listen(9000);
