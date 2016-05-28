05658f653a5923d89438a8746886248f329566e9
Denis Missias <denis.missias@gmail.com>
2016-05-27 21:04:57 -0300

    Finalização do capítulo 2

--- Build App NodeJS/Chapter 2/server-http.js
+++ Build App NodeJS/Chapter 2/server-http.js
@@ -2,7 +2,14 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello World\n');
+
+	if (request.url === '/') {
+		response.end('Hello World\n');
+	} else if (request.url === '/contato') {
+		response.end('denis.missias@gmail.com\n');
+	} else {
+		response.end('Not found');
+	}
 });
 
 server.listen(1337);
