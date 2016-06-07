d4640c410f3b82c343809d032d13311b918c9c2a
Denis de Sousa Missias <denismissias@hotmail.com>
2016-06-06 21:49:18 -0300

    Adding a Model

---
+++ ASPNET/MVC5/MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public Int32 ID { get; set; }
+        public String Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public String Genre { get; set; }
+        public Decimal Price { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
