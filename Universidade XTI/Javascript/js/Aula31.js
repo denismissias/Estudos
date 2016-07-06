4f3908f564a725a8b9edb41f27104ab4a16f8a8a
denismissias <denis.missias@gmail.com>
2016-07-05 21:21:41 -0300

    Exemplos Objeto Math

---
+++ Universidade XTI/Javascript/js/Aula31.js
@@ -0,0 +1,13 @@
+//alert(2 * Math.PI * 30);
+//
+//alert(Math.abs(-3));
+//
+//alert(Math.max(2, 3.7, -5, 9, 1));
+//
+//alert(Math.round(3.4));
+
+//Arredonda para baixo
+alert(Math.floor(3.3));
+
+//Arredonda para cima
+alert(Math.ceil(3.3));
